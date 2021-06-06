ae70640668d974918da1b06bbe5e9ad823e73ce9
Nailuj29 Gaming <nailuj29@nailuj29gaming@gmail.com>
2021-06-06 15:48:46 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,17 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my website" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I am nailuj. I make <a href="/stuff-ive-made">stuff</a> sometimes
         </p>
+        <h2>Crypto:</h2>
+        RVN: <pre>REMmZ47rwFZPumajKJvqVgFzZqwnz7hyg6</pre> <br />
+        DOGE: <pre>DFUiTucjnJp1kvbF3F4TF7CR2nxS74SGMy</pre> <br />
+        ETH: <pre>0xfABa9C1c9c36fC0b2A4F90a31eaA54Db3c44eE2B</pre> <br />
+        <br />
+        <Footer />
       </main>
-
-      <Footer />
     </div>
   )
 }
---
+++ pages/stuff-ive-made.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Stuff() {
+  return (
+    <div className="container">
+        <Head>
+            <title>Next.js Starter!</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+            <Header title="Stuff I've made" />
+            <h2>Esolangs</h2>
+            <ul>
+                <li><a href="https://github.com/LengthLang/length">Length</a></li>
+            </ul>
+            <h2>Web Stuff</h2>
+            <ul>
+                <li><a href="https://github.com/nailuj29gaming/paste">Nailujbin</a></li>
+                <li><a href="https://github.com/SpiderWren/SpiderWren">SpiderWren</a></li>
+            </ul>
+            <h2>Discord Bots</h2>
+            <ul>
+                <li><a href="https://top.gg/bot/752236008497479821">DAndDBot</a></li>
+                <li><a href="https://top.gg/bot/535636428449644552#/">PollsBot</a></li>
+                <li><a href="https://github.com/nailuj29gaming/join-leave">JoinLeave (too lazy for verification)</a></li>
+            </ul>
+            <Footer />
+        </main>
+    </div>
+  )
+}
